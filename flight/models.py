b6b75456fec6c378a48a74ca68fdbc02f653d476
Vika <viktorykoroliova@gmail.com>
2023-01-25 18:09:55 +0200

    Created model DesignBureau with fields name and headquarter

--- flight/models.py
+++ flight/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Position(models.Model):
     title = models.CharField(max_length=255)
 
+
+class DesignBureau(models.Model):
+    name = models.CharField(max_length=255)
+    headquarter = models.CharField(max_length=255)
+
