e839e6dbd9c281cc0d72716bd8ad1213dfb14298
Vika <viktorykoroliova@gmail.com>
2023-01-25 18:11:49 +0200

    Created model CrewMember inherited from AbstractUser with fields first_name, last_name, license_number and position (ForeignKey to model Position)

--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -9,3 +10,9 @@ class DesignBureau(models.Model):
     name = models.CharField(max_length=255)
     headquarter = models.CharField(max_length=255)
 
+
+class CrewMember(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=63)
+    position = models.ForeignKey(to=Position, on_delete=models.CASCADE, null=True, related_name="crew")
