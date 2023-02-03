50a2316f8d19483542cc159ccc05d94d4fd587e9
Vika <viktorykoroliova@gmail.com>
2023-02-03 19:57:59 +0200

    Removed insecure Django secret key from settings

--- flight_manager/settings.py
+++ flight_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-%4p1pny#()qar0tvl5ghwv#$_tr!6lzn2%ox+=dxog6sq99bvw")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
