b3b927a2c91865450f5bdbe3bdddfbe38a4ad8af
Vika <viktorykoroliova@gmail.com>
2023-02-03 15:05:53 +0200

    Updated settings and requirements.txt

--- flight_manager/settings.py
+++ flight_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -21,15 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-%4p1pny#()qar0tvl5ghwv#$_tr!6lzn2%ox+=dxog6sq99bvw"
-)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-%4p1pny#()qar0tvl5ghwv#$_tr!6lzn2%ox+=dxog6sq99bvw")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
