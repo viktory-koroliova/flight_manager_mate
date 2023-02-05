f1ee92a4c3ea6c49238b802745b8c8b195133e22
Vika <viktorykoroliova@gmail.com>
2023-02-05 14:29:28 +0200

    Added flight-manager.onrender.com to ALLOWED_HOSTS

--- flight_manager/settings.py
+++ flight_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://flight-manager.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
